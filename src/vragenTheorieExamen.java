dd4f2a85b1822612b9a8c33a7b5920c6ddc8634c
Spicyflam <mohamedvoetballer21@gmail.com>
2021-03-10 21:21:52 +0100

    Snelle methode
    kleine methode

--- src/vragenTheorieExamen.java
+++ src/vragenTheorieExamen.java
@@ -1,8 +1,10 @@
 public class vragenTheorieExamen {
 
     private int punten= 0;
+    public static void main(String args[]) {
 
-    System.out.println("Welkom bij de TheorieExamen")
+        System.out.println("Welkom bij de TheorieExamen");
+    }
 
 
 }
