636bd98e4e84a259432641cc4dc52d02651b21c0
Spicyflam <mohamedvoetballer21@gmail.com>
2021-03-10 21:54:09 +0100

    Misschien helpt dit nilesh
    Laatste beetje code

--- src/vragenTheorieExamen.java
+++ src/vragenTheorieExamen.java
@@ -5,20 +5,26 @@ public class vragenTheorieExamen {
     private int punten= 0;
     public static void main(String args[]) {
 
-        System.out.println("Welkom bij de TheorieExamen");
+
     }
 
-    public static void examensAfnemen(){
+    public static void examensAfnemen() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("1) Theorie examen");
         System.out.println();
         System.out.println("2) Praktijk examen");
         System.out.println();
         System.out.printf("Kies uw examen:");
-
-
+    }
+    public void TheorieExamen()
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welkom bij de TheorieExamen");
 
 
     }
+
+
+
 }
 
