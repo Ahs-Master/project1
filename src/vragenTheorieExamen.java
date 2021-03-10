7b347c6e1c99379e17ecefb73c3e4f4ff5ab5d0b
NiliXCIX <nilesh-bahoran@hotmail.com>
2021-03-10 21:26:22 +0100

    update

--- src/vragenTheorieExamen.java
+++ src/vragenTheorieExamen.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class vragenTheorieExamen {
 
     private int punten= 0;
@@ -6,5 +8,17 @@ public class vragenTheorieExamen {
         System.out.println("Welkom bij de TheorieExamen");
     }
 
+    public static void examensAfnemen(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1) Theorie examen");
+        System.out.println();
+        System.out.println("2) Praktijk examen");
+        System.out.println();
+        System.out.printf("Kies uw examen:");
 
+        if
+
+
+    }
 }
+
