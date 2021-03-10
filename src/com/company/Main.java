8bf3bf58658cd0f7ad87fcc94154f97bd15d80fc
Ahs-Master <80402711+Ahs-Master@users.noreply.github.com>
2021-03-10 18:07:03 +0100

    beginscherm
    beginscherm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        beginScherm();
+    }
 
-        
+    public static void beginScherm() {
+        System.out.println("Menu");
+        System.out.println();
+        System.out.println("1) Lijst met examens");
+        System.out.println();
+        System.out.println("2) Lijst met studenten");
+        System.out.println();
+        System.out.println("3) Nieuwe student inschrijven");
+        System.out.println();
+        System.out.println("4) Student verwijderen");
+        System.out.println();
+        System.out.println("5) Examen afnemen");
+        System.out.println();
+        System.out.println("6) Welke examens heeft student gehaald?");
+        System.out.println();
+        System.out.println("7) Welke student heeft de meeste examens gehaald?");
+        System.out.println();
+        System.out.println("0) Afsluiten");
+        System.out.println();
+        System.out.println("Uw keuze: ");
+
     }
 }
