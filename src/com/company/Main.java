ef837df98389fbc0cec376972fa00e3ed56e15c6
Ahs-Master <80402711+Ahs-Master@users.noreply.github.com>
2021-03-11 09:29:56 +0100

    Update ochtend 11-03-2021

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -38,7 +38,7 @@ public class Main {
         System.out.println();
         System.out.println("0) Afsluiten");
         System.out.println();
-        System.out.println("Uw keuze: ");
+        System.out.printf("Uw keuze: ");
 
         int keuze;
 
