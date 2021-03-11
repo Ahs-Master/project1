2ae39cb917318658c25a7dbd8b9dad56e0bcd640
Ahs-Master <80402711+Ahs-Master@users.noreply.github.com>
2021-03-11 12:27:52 +0100

    veel submenus toegevoegd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,6 +16,7 @@ public class Main {
         naam = scanner.next();
         beginScherm();
     }
+
     public static void beginScherm() {
         Scanner scanner = new Scanner(System.in);
         System.out.println();
@@ -48,13 +49,13 @@ public class Main {
                 if (keuze == 1) {
                     examenLijst();
                 } else if (keuze == 2) {
-                    System.out.println("ga naar menu van 2");
+                    studentenLijst();
                 } else if (keuze == 3) {
-                    System.out.println("ga naar menu van 3");
+                    studentInschrijven();
                 } else if (keuze == 4) {
                     System.out.println("ga naar menu van 4");
                 } else if (keuze == 5) {
-                    System.out.println("ga naar menu van 5");
+                    examenAfnemen();
                 } else if (keuze == 6) {
                     System.out.println("ga naar menu van 6");
                 } else if (keuze == 7) {
@@ -62,12 +63,12 @@ public class Main {
                 } else if (keuze == 0) {
                     System.out.println("ga naar menu van 0");
                 } else if (keuze != 1 || keuze != 2 || keuze != 3 || keuze != 4 || keuze != 5 || keuze != 6 || keuze != 7 || keuze != 0) {
-                    System.out.println("Kies aub een menu... Druk op Enter om verder te gaan");
+                    System.out.printf("Kies aub een menu... Druk op Enter om verder te gaan");
                     scanner.nextLine();
                     beginScherm();
                 }
             } else {
-                System.out.println("Voer een getal in! Probeer het opnieuw. Druk Enter");
+                System.out.printf("Voer een getal in! Probeer het opnieuw. Druk Enter");
                 scanner.nextLine();
                 scanner.nextLine();
                 beginScherm();
@@ -83,7 +84,7 @@ public class Main {
         System.out.println();
         System.out.println("-Praktijk Examen");
         System.out.println();
-        System.out.println("1) Terug naar hoofdmenu");
+        System.out.printf("1) Terug naar hoofdmenu");
 
         int keuze;
 
@@ -116,6 +117,11 @@ public class Main {
         System.out.println("Mansori Derksen");
         System.out.println("Johan Baardemans");
         System.out.println("Gert Jan");
+        /*
+        Hier komen namen van studenten die in de Class van Rachid staan
+
+
+        */
         System.out.println();
         System.out.println("1) Terug naar hoofdmenu");
 
@@ -140,6 +146,80 @@ public class Main {
             studentenLijst();
         }
 
+    }
+
+    public static void studentInschrijven () {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println();
+        System.out.println("1) Student Inschrijven");
+        System.out.println();
+        System.out.println("2) Terug naar hoofdmenu");
+
+        int keuze;
+        if (scanner.hasNextInt()) {
+            keuze = scanner.nextInt();
+            scanner.nextLine();
+            if (keuze == 1) {
+                //ga naar de Class van Rachid waar je student inschrijft
+            } else if (keuze == 2) {
+                beginScherm();
+            } else {
+                System.out.println("Kies aub een menu, druk op Enter om opnieuw te proberen.");
+                scanner.nextLine();
+                scanner.nextLine();
+                studentInschrijven();
+            }
+        } else {
+            System.out.println("Voer een getal in. Druk op Enter om opnieuw te proberen");
+            scanner.nextLine();
+            scanner.nextLine();
+            studentInschrijven();
+        }
+
+
+    }
+
+    public static void studentVerwijderen () {
+        /*
+
+Class van rachid student verwijdern met arraylist.remove(naam) en checken of de student er wel echt in is dus met  arraylist.contains(naam)
+
+
+         */
+    }
+
+    public static void examenAfnemen () {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welke examen wil je afnemen?");
+        System.out.println();
+        System.out.println("1) Theorie-Examen");
+        System.out.println();
+        System.out.println("2) Praktijk-Examen");
+        System.out.println();
+        System.out.println("3) Terug naar hoofdmenu");
+
+        int keuze;
+        if (scanner.hasNextInt() == true) {
+            keuze = scanner.nextInt();
+            scanner.nextLine();
+            if (keuze == 1) {
+                //Theorie Examen Class executeren
+            } else if (keuze == 2) {
+                //Praktijk Examen Class executeren
+            } else if (keuze == 3) {
+                beginScherm();
+            } else {
+                System.out.println("Kies aub een menu, druk Enter om verder te gaan");
+                scanner.nextLine();
+                examenAfnemen();
+            }
+
+        } else {
+            System.out.println("Voer een getal in, druk op Enter om opnieuw te proberen");
+            scanner.nextLine();
+            scanner.nextLine();
+            examenAfnemen();
+        }
     }
 
 
