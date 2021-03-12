251b5bc0fc287de244f609ce58f4b2fa95e9b1f5
Rachid Angelista <mrachid.ang@gmail.com>
2021-03-13 00:54:55 +0100

    Testing a few stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
 
 
@@ -24,11 +27,33 @@ public class Main {
         System.out.println("Welkom, BozeNeefo,");
         System.out.println();
         System.out.println();
-        System.out.println("Druk op enter om door te gaan.");
+        System.out.println("Druk op enter om naar de menu te gaan.");
+        pressAnyKeyToContinue();
+        clearConsole();
 
 
 
-        clearScreen();
+        System.out.println("Welkom bij (TeamNaam) RijSchool!");
+        System.out.println();
+        System.out.println("Wilt u inloggen als Student of Docent?");
+        System.out.println();
+        System.out.println("1).Student");
+        System.out.println("2).Docent");
+        System.out.println();
+        System.out.println("U heeft voor docent gekozen. Om in te loggen als docent, voer in uw gebruikersnaam en password");
+        System.out.println();
+        System.out.println();
+        System.out.println("Gebruikersnaam: BozeNeefo");
+        System.out.println("Wachtwoord:     ZinnaPatrool");
+        System.out.println();
+        System.out.println();
+        System.out.println("Welkom, BozeNeefo,");
+        System.out.println();
+        System.out.println();
+        System.out.println("Druk op enter om naar de menu te gaan.");
+
+
+
 
 
 
@@ -40,9 +65,23 @@ public class Main {
 
 
     }
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+    public static void clearConsole(){
+
+        for (int i = 0; i < 50; i++){
+            System.out.println("");
+        }
+
+
+    }
+
+
+
+    private static void pressAnyKeyToContinue()
+    {
+        try {System.in.read();
+        }
+        catch (Exception e){
+        }
     }
 
 
