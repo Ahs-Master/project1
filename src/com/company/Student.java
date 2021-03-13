74f873129e70d70c31fd8f9204811dc7070c1d34
Rachid Angelista <80318554+R-Cloud-Designs@users.noreply.github.com>
2021-03-13 08:44:48 +0100

    Student Class geCommented, met uileg.

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -6,34 +6,33 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Student {
 
+    //<------Instance Variables------->
     private final String naam;
     private final String achternaam;
     private static Integer studentId = 20130767;
     private static boolean geslaagd;
 
-
+    //<------Listen van O.A Student, GeslaagdeStudenten,StudentIdLijst------->
     private static final ArrayList<Student> studentLijst = new ArrayList<>();
     private static final ArrayList<Student> geslaagdeStudenten = new ArrayList<>();
     private static final ArrayList<Integer> studentenIdLijst = new ArrayList<>();
 
     //<<----Constructor---->>
-
     public Student(String naam, String achternaam){
 
         this.naam = naam;
         this.achternaam =  achternaam;
 
-
-
-
     }
 
-    //<----Accessors----->
-
+    //<------Nieuwe Student aanmakken------->
     public static void addStudent(String naam, String achternaam){
+        //<----------add aangemaakte student to studentlijst---------->
         Student.studentLijst.add(new Student(naam,achternaam));
 
-        //Generate a Random Number for Student elke keer dat een nieuwe student aangemaakt wordt.
+        /*Generate a Random Number for StudentID elke keer dat een nieuwe student aangemaakt wordt. als de studentID al een keer
+        voorkomt, wordt er een nieuwe number gemaakt.*/
+
         for (Integer integer : studentenIdLijst) {
             if (integer.equals(studentId)) {
                 studentId = ThreadLocalRandom.current().nextInt(20130767, 20130999);
@@ -41,6 +40,8 @@ public class Student {
         }
         studentenIdLijst.add(studentId);
     }
+
+    //<----Om Studentenlijst uit te printen(Student en Idnummer)------->
     public static void printStudentLijst() {
         for (int i = 0; i < studentLijst.size(); i++ ) {   // de studenten uitprinten op scherm.
 
@@ -48,6 +49,8 @@ public class Student {
             System.out.printf("Student Nummer: %d%n%n",studentenIdLijst.get(i));
         }
     }
+
+    //<----Mutators------->
     public static void setWelGeslaagd(Student student){
         geslaagd = true;
         geslaagdeStudenten.add(student);
@@ -57,7 +60,7 @@ public class Student {
         geslaagd = false;
     }
 
-    //<----Mutators------->
+    //<----Accessors------->
     public String getNaam() {
         return naam;
     }
