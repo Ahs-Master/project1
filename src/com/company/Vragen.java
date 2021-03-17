dc3c0ad64b5bc831762af7cc1b1b06798d3f3544
NiliXCIX <nilesh-bahoran@hotmail.com>
2021-03-17 22:29:40 +0100

    Een begin aan class Vragen

---
+++ src/com/company/Vragen.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Vragen {
+    public static String vraag;
+    public static String antwoord;
+
+    public static ArrayList <String> Vragen = new ArrayList<>();
+
+    public void Toetsvragen (String vraag, String antwoord){
+        this.vraag = vraag;
+        this.antwoord = antwoord;
+    }
+    public String getVraag(){ return this.vraag; }
+    public String getAntwoord(){ return this.antwoord; }
+}
