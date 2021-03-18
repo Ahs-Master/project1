873272f4c0a7a864d3a35ab42c9f17afabff0850
Rachid Angelista <mrachid.ang@gmail.com>
2021-03-18 16:57:42 +0100

    Gebruiker Branche is klaar voor nu

--- src/com/company/ExamenTabel.java
+++ src/com/company/ExamenTabel.java
@@ -1,17 +1,19 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class ExamenTabel {
 
-    static String examenNaam;
-    static Integer aantalPogingen;
-    static Integer aantalpunten;
-    //ArrayList<Vragen> examenVragen;
+    public static String examenNaam;
+    public static Integer aantalPogingen;
+    public static Integer aantalpunten;
+    public static ArrayList <Vragen> examenVragen;
 
 
     public ExamenTabel(String examenNaam)
     {
         this.examenNaam = examenNaam;
-        //examenVragen.add(Vragen);
+        examenVragen.add(Vragen);
     }
 
     public void beginExamen()
@@ -22,15 +24,14 @@ public class ExamenTabel {
     public void setStudentIsGeslaagd(Student student)
     {
 
-        if(aantalpunten>=15)
+        if(aantalpunten>=11)
         {
             Student.setWelGeslaagd(student);
-            System.out.println("Je bent geslaagd voor de toets!");
+            System.out.printf("Je bent geslaagd voor de toets %s!",ExamenTabel.examenNaam);
 
         }
 
-        else if(aantalpunten<=14)
-        {
+        else {
             System.out.println("Helaas, je bent gezakt");
         }
     }
