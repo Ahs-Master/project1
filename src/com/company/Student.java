8ceb88ecf469d4356b00189f9309b8267f657a91
Rachid Angelista <mrachid.ang@gmail.com>
2021-03-18 05:07:56 +0100

    Gebruiker Branche is klaar voor nu

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -4,34 +4,19 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 
-public class Student {
+public class Student extends Gebruiker {
 
-    //<------Ins+tance Variables------->
-    private final String naam;
-    private final String achternaam;
+   // <------Instance Variables------->
     private static Integer studentId = 20130767; // StudentID moet ergens beginnen.
-    private static boolean geslaagd;
-
-    //<------Listen van O.A Student, GeslaagdeStudenten,StudentIdLijst------->
-    private static final ArrayList<Student> studentLijst = new ArrayList<>();
-    private static final ArrayList<Student> geslaagdeStudenten = new ArrayList<>();
+    private static boolean isGeslaagd = false;
+    private static final ArrayList<Student> studentenLijst = new ArrayList<>();
     private static final ArrayList<Integer> studentenIdLijst = new ArrayList<>();
+    private static final ArrayList<Student> geslaagdeStudenten = new ArrayList<>();
 
     //<<----Constructor---->>
-    public Student(String naam, String achternaam){
+    public Student(String naam, String achternaam) {
+        super(naam, achternaam);
 
-        this.naam = naam;
-        this.achternaam =  achternaam;
-
-    }
-
-    //<------Nieuwe Student aanmakken------->
-    public static void addStudent(String naam, String achternaam){
-        //<----------add aangemaakte student to studentlijst---------->
-        Student.studentLijst.add(new Student(naam,achternaam));
-
-        /*Generate a Random Number for StudentID elke keer dat een nieuwe student aangemaakt wordt. als de studentID al een keer
-        voorkomt, wordt er een nieuwe number gemaakt.*/
 
         for (Integer K : studentenIdLijst) {
             if (K.equals(studentId)) {
@@ -39,25 +24,23 @@ public class Student {
             }
         }
         studentenIdLijst.add(studentId);
+        studentenLijst.add(this);
     }
 
-    //<----Om Studentenlijst uit te printen(Student en Idnummer)------->
-    public static void printStudentLijst() {
-        for (int i = 0; i < studentLijst.size(); i++ ) {   // de studenten uitprinten op scherm.
 
-            System.out.printf("Student Naam: %s %s%n",studentLijst.get(i).getNaam(),studentLijst.get(i).getAchternaam());
+    public static void printStudentLijst() {
+        for (int i = 0; i < studentenLijst.size(); i++ ) {   // de studenten uitprinten op scherm.
+
+            System.out.printf("Student Naam: %s %s%n",studentenLijst.get(i).getNaam(),studentenLijst.get(i).getAchternaam());
             System.out.printf("Student Nummer: %d%n%n",studentenIdLijst.get(i));
         }
     }
 
     //<----Mutators------->
     public static void setWelGeslaagd(Student student){
-        geslaagd = true;
+        isGeslaagd = true;
         geslaagdeStudenten.add(student);
 
-    }
-    public static void setNietGeslaagd(){
-        geslaagd = false;
     }
 
     //<----Accessors------->
@@ -72,4 +55,8 @@ public class Student {
     }
 
 }
+
+
+
+
 
