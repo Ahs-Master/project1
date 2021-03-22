bd2eadf8da62a7f41ea7b1a363f2c07cd032c218
NiliXCIX <nilesh-bahoran@hotmail.com>
2021-03-22 10:56:44 +0100

    update

--- src/com/company/OpenVragen.java
+++ src/com/company/OpenVragen.java
@@ -1,16 +1,14 @@
 package com.company;
 
-import java.util.Scanner;
 
 public class OpenVragen extends Vragen {
-    Scanner scanner = new Scanner(System.in);
-    String antwoord1 = scanner.hasNext();
-    public boolean CheckVragen(){
+
+    public String CheckVragen(){
         if (antwoord.equals(antwoord1)){
-            return true;
+            return "Goed";
         }
         else {
-            return false;
+            return "Fout";
         }
     }
 }
