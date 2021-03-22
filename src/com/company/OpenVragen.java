aeec67c492e36aa9f82a84c57302cfb78bd4ce1a
Rachid Angelista <mrachid.ang@gmail.com>
2021-03-22 13:27:37 +0100

    Gebruiker Branche is klaar voor nu

--- src/com/company/OpenVragen.java
+++ src/com/company/OpenVragen.java
@@ -3,12 +3,19 @@ package com.company;
 
 public class OpenVragen extends Vragen {
 
-    public String CheckVragen(){
-        if (antwoord.equals(antwoord1)){
+    private static String aantwoord;
+
+    public String CheckVragen(Vragen vraag){
+        if (vraag.getAntwoord().equals(antwoord)){
             return "Goed";
         }
         else {
             return "Fout";
         }
+    }
+
+    private static void setAantwoord(String aAntwoord){
+        aantwoord = aAntwoord;
+
     }
 }
