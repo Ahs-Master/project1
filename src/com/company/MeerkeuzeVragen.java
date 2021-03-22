cf99c06e292a798f78de220cdfd9e9a21a229837
NiliXCIX <nilesh-bahoran@hotmail.com>
2021-03-22 10:50:48 +0100

    update

--- src/com/company/MeerkeuzeVragen.java
+++ src/com/company/MeerkeuzeVragen.java
@@ -1,19 +1,25 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MeerkeuzeVragen extends Vragen {
-    public static String[] keuzes = new ArrayList<>();
+    public static ArrayList<String> keuzes = new ArrayList<>();
 
     public void meerkeuzevragen (String vraag, String[] keuzes, String antwoord){
         this.vraag = vraag;
         this.antwoord = antwoord;
-        this.keuzes = new ArrayList<>();
+        this.keuzes = new ArrayList<>()
+        ;
+        this.keuzes.addAll(Arrays.asList(keuzes));
+
+        Collections.shuffle(this.keuzes);
 
         Vragen.add(vraag);
     }
 
-    public String[] getKeuzes() {
+    public ArrayList<String> getKeuzes() {
         return keuzes;
     }
 }
--- src/com/company/OpenVragen.java
+++ src/com/company/OpenVragen.java
@@ -1,5 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class OpenVragen extends Vragen {
-    
+    Scanner scanner = new Scanner(System.in);
+    String antwoord1 = scanner.hasNext();
+    public boolean CheckVragen(){
+        if (antwoord.equals(antwoord1)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
