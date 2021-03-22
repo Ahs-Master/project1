9869551d4ee52fd6498daef59b45a408d1c73fa3
NiliXCIX <nilesh-bahoran@hotmail.com>
2021-03-22 10:06:23 +0100

    update vragen

--- src/com/company/MeerkeuzeVragen.java
+++ src/com/company/MeerkeuzeVragen.java
@@ -1,12 +1,16 @@
 package com.company;
 
-public class MeerkeuzeVragen extends Vragen {
-    public static String [] keuzes = new String[];
+import java.util.ArrayList;
 
-    public meerkeuzevragen (String vraag, String[] keuzes, String antwoord){
+public class MeerkeuzeVragen extends Vragen {
+    public static ArrayList <String> keuzes = new ArrayList<>();
+
+    public void meerkeuzevragen (String vraag, String[] keuzes, String antwoord){
         this.vraag = vraag;
-        this.keuzes = keuzes;
         this.antwoord = antwoord;
+        this.keuzes = new ArrayList<>();
+
+        Vragen.add(vraag);
     }
 
     public String[] getKeuzes() {
