8f736b397de61a1c82d01d128270ea5141f2cf9c
Rachid Angelista <mrachid.ang@gmail.com>
2021-03-24 18:45:30 +0100

    Gebruiker Branche is klaar voor nu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Student Kevin = new Student("Rachid","Angelista");
-        Student Nilesh = new Student("Nilesh","Bahoran");
-        Student TEST = new Student("Rachid","Bahoran");
 
-        Docent Mo = new Docent("Mohamed","Barbouch");
-
-
-        Student.printStudentLijst();
     }
 }
